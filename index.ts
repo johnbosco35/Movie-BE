3ba30b9f2d8c1c01a309484fbc30c48926848d65
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-07-31 00:39:49 +0100

    added something in my index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 import express,{Application} from "express"
-import dotenv from "dotenv"
+import { environmentVariable } from "./src/env/environment"
 
-dotenv.config()
+
+
 
 const app:Application = express()
 
-const port:number = 2024
+const port:Number | any = process.env.port
+
+const server = app.listen(environmentVariable.port,() =>{
+console.log("Server is now on")
+})
+
+process.on("unhandledRejection", (error:any) =>{
+    console.log("unhandledRejection",error);
+    process.exit(1);
+});
+process.on("uncaughtException", (reason:any) =>{
+    console.log("uncaughtException",reason);
+
+    server.close(()=>{
+        process.exit(1);
+    })
+})
+
+
