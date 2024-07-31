950e2ce66a3e2e7b1505675cb0de259f399b93e5
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-07-31 01:25:09 +0100

    just created and added some codes in my DB.ts file

---
+++ src/config/DB.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { environmentVariable } from "../env/environment";
+
+const URI:any = environmentVariable.mongodb_string;
+
+export const DBconnect = () =>{
+    try {
+        const connect = mongoose.connect(URI);
+        console.log("MongoDB connected");;
+    } catch (error) {
+        console.log(error)
+    }
+}
