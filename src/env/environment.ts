f757fd7ed7e75f5c391a4ecaa64ffe577792a04a
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-08-03 13:37:54 +0100

    added

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const environmentVariable = {
     port: process.env.port!,
     mongodb_string: process.env.mongoDB_string! as any,
-    JWT_SECRE: process.env.JWT_SECRE!,
+    JWT_SECRET: process.env.JWT_SECRET!,
     client_id: process.env.client_id!,
     clientSecret: process.env.clientSecret!,
     callbackUrl2: process.env.callbackUrl2!,
